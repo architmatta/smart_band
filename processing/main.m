dbf4201518cb237d5607997df84efe689d57630a
Ayush Kumar <ayushkr79@gmail.com>
2017-01-23 13:04:58 +0530

    Done

--- processing/main.m
+++ processing/main.m
@@ -28,9 +28,10 @@ amp = amp';
 time = time';
 mat = [max_freq, amp];
 [freq,~,ind] = unique(mat(:,1));
-freq_amp = [freq, accumarray(ind,mat(:,2))];
+freq_amp = [freq, abs(accumarray(ind,mat(:,2)))];
 
 fprintf('Plotting the dominant frequency and its A value....\n');
+freq_amp = sortrows(freq_amp, -2);
 scatter(freq_amp(:, 1), abs(freq_amp(:, 2)));
 
 %% ======================= Part 3:   =======================
